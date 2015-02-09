db66b4b2ce8f610920106c89fbbaa254e11f135c
Mustafa Karakaplan <karakaplanm@users.noreply.github.com>
2015-02-09 09:50:11 +0200

    Update hellogui.c

--- hellogui.c
+++ hellogui.c
@@ -1,3 +1,6 @@
+// Windowsapi ile pencere ve buton oluşturma
+//Derlemek için: gcc hello.c -o hello.exe
+
 #include <windows.h>
 
 int APIENTRY WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,LPSTR lpCmdLine, int nCmdShow)
