5edd0cd150ec4b56e72235377ba8057308772cd4
Mustafa Karakaplan <karakaplanm@users.noreply.github.com>
2015-02-09 09:42:11 +0200

    First Commit

---
+++ hellogui.c
@@ -0,0 +1,7 @@
+#include <windows.h>
+
+int APIENTRY WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,LPSTR lpCmdLine, int nCmdShow)
+{
+    MessageBox(NULL,"Bu bir Penceredir","Hello World", MB_OK);
+    return 0;
+}
